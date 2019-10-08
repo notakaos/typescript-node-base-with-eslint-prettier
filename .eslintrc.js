2cf70fc43c4864d3211dccd4ca2b36a965c04762
Nobutaka OSHIRO <notakaos@notakaos.com>
2019-10-08 13:57:05 +0900

    Add eslint & @typescript-eslint/plugin

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2019
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+};
